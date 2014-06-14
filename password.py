33fd0401519293133850e09245f0f70aeb48d784
silverp1 <jpettit@pettitservers.com>
2014-06-14 07:13:14 +0000

    Minor changes to index

--- password.py
+++
@@ -1,7 +0,0 @@
-import md5
-
-def hashPassword(password):
-	m = md5.new()
-	m.update(password)
-	m.update("TGFQJH#()*tfgHQA@EghAEUJgvb A#W)EQA#RT#QUTGHE@Q*RFHQRQFDsifjaij@D*()q238r9q3e4#)*@(%$&!^@)#*!@&#")
-	return m.hexdigest()
