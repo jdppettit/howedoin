a1d6876643aa0f74b5312a1f4c8c3ac5985b2551
silverp1 <jpettit@pettitservers.com>
2014-08-04 23:51:52 +0000

    Added some additional logic to register function

--- register.py
+++ register.py
@@ -12,6 +12,22 @@ def register():
                 # if session vars not found (not logged in)
                 return render_template("register.html")
         elif request.method == "POST":
+            if request.form['username'] and request.form['name'] and request.form['email'] and request.form['password']
+            and request.form['passwordconfirm'] and request.form['plan']:
+                if request.form['plan'] == 0:
+                    # if plan is free, don't do billing, paidthru is forever
+                    if request.form['company_name']:
+                        # if company_name is there, use it    
+                    else:
+                        # if its not, provide empty string
+                else:
+                    # do billing, paidthru should be one month from now
+                    if request.form['company_name']:
+                        # if company name us it
+                    else:
+                        # if not use empty string
+            else:
+                return render_template("register.html", error="Please fill out all of the required fields.")
             # do all the registration stuff
             # if not on free plan, pass to billing
             # if on free plan, redirect to dashboard
