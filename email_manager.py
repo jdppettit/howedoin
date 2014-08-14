50608454f70002308dc32608dfa184c18bc303d1
silverp1 <jpettit@pettitservers.com>
2014-08-14 01:15:32 +0000

    Implemented the function to send activation email as well as the endpoint and code to handle new user activations

--- email_manager.py
+++ email_manager.py
@@ -7,8 +7,11 @@ mail = Mail()
 def sendWelcome(email_address, account_id):
     return 1
 
-def sendCreateNewUser(email_address, activation_code, account_id, user_id):
-    return 1
+def sendCreateNewUser(email_address, activation_code, account_id, user_id, name):
+    msg = Message("You have been invited to Howedoin", sender = "donotreply@howedo.in", recipients = [email_address])
+    msg.html = render_template("email_create_new_user.html", activation_code=activation_code, user_id=user_id,
+    account_id=account_id, name=name)
+    mail.send(msg) 
 
 def sendForgotPassword(email_address, token, account_id, user_id):
     return 1
--- user.py
+++ user.py
@@ -2,6 +2,7 @@ from flask import *
 from models import db, User, Account, Team
 from functions import *
 from email_manager import *
+from password import *
 
 import string
 import random
@@ -21,11 +22,25 @@ def getActivationURL(size=6, chars=string.ascii_uppercase + string.digits):
 
 def makeUser(account_id, name, username, email, active, activation_link):
     password = getActivationURL(25)
-    newUser = User(account_id, name, username, password, email, "", activation_link)
+    newUser = User(account_id, name, username, password, email, 0, activation_link)
     db.session.add(newUser)
     db.session.commit()
     db.session.refresh(newUser)
     return newUser.id
+
+def checkToken(token):
+    user = User.query.filter_by(activation_link=token).first()
+    if user:
+        return True, user
+    else:
+        return False, False
+
+def updateActivatedUser(password, token):
+    user = User.query.filter_by(activation_link=token).first()
+    user.password = password
+    user.active = 1
+    db.session.commit()
+    return user
 
 @user.route('/dashboard/user/')
 def viewUsers():
@@ -35,6 +50,27 @@ def viewUsers():
         return render_template("dashboard_user_view.html", users=allUsers)
     else:
         return notLoggedIn()
+
+@user.route('/user/activate/<token>', methods=['POST','GET'])
+def activateUser(token):
+    if token:
+        res, User = checkToken(token)
+        if res:
+            if request.method == "GET":
+            # token is good, lets do the needful
+                return render_template("activate_user.html", token=token)
+            elif request.method == "POST":
+                if request.form['password'] == request.form['passwordconfirm']:
+                    newPassword = hashPassword(request.form['password'])
+                    user = updateActivatedUser(newPassword, token)
+                    doLogin(user)
+                    return redirect('/dashboard')
+                else:
+                    return render_template("activate_user.html", token=token, error="Your passwords do not match. Please try again!")
+        else:
+            return render_template("done.html", error="That token does not match a user.")
+    else:
+        return render_template("index.html")
 
 @user.route('/dashboard/user/create', methods=['POST','GET'])
 def createUser():
@@ -48,7 +84,7 @@ def createUser():
                 activation_code = getActivationURL(10)
                 user_id = makeUser(session['account_id'], request.form['name'], request.form['username'],
                 request.form['email'], 0, activation_code)
-                sendCreateNewUser(request.form['email'], activation_code, session['account_id'], user_id)
+                sendCreateNewUser(request.form['email'], activation_code, session['account_id'], user_id, request.form['name'])
                 return render_template("dashboard_user_view.html", message="Activation email sent to this user.")
             else:
                 return render_template("dashboard_user_create.html", error="All fields must be completed.")
